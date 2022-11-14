962c1eb8f9905b12c035d89ee373d7c5fcc693b9
Maximilien Drules <maximilien.drules@epitech.eu>
2022-11-14 20:47:01 +0100

    chore: update Makefile and index.h

--- lib/my/my.h
+++ lib/my/my.h
@@ -1,4 +1,7 @@
 #ifndef MY_H_
 #define MY_H_
 
+#include "linked_list/index.h"
+#include "std/index.h"
+
 #endif /* MY_H_ */
---
+++ lib/my/std/index.h
@@ -0,0 +1,16 @@
+#ifndef STD_H_
+#define STD_H_
+
+#include "atoi/my_atoi.h"
+#include "memcpy/my_memcpy.h"
+#include "memset/my_memset.h"
+#include "realloc/my_realloc.h"
+#include "split/my_str_to_word_array.h"
+#include "strcat/my_strcat.h"
+#include "strcmp/my_strcmp.h"
+#include "strdup/my_strdup.h"
+#include "strlen/my_strlen.h"
+#include "strncmp/my_strncmp.h"
+#include "strsep/my_strsep.h"
+
+#endif /* STD_H_ */
